a0f3fce1280beefdce2278644e6f2914fe0f6b26
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-11-23 18:39:37 +0300

    Add basic auhtorization page

--- app/src/main/java/com/example/moodwave/MainActivity.java
+++ app/src/main/java/com/example/moodwave/MainActivity.java
@@ -8,17 +8,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.EditText;
+
 public class MainActivity extends AppCompatActivity {
+    private EditText editUsername;
+    private EditText editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editUsername = findViewById(R.id.editUsernameAuthorization);
+        editPassword = findViewById(R.id.editUsernamePassword);
+
+        findViewById(R.id.buttonAuthorization).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
         });
+    }
+
+    private void login(){
+        System.out.println("It`s alive");
     }
 }
